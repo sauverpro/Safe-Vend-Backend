7998bc4e0edce549f2c9edc367c567984a6788f7
sauverpro <unsauvechriss@gmail.com>
2025-09-13 12:49:05 +0200

    port

--- config/database.js
+++ config/database.js
@@ -4,11 +4,13 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 const sequelize = new Sequelize(
-  process.env.DB_NAME || 'condom_vending',
-  process.env.DB_USER || 'root',
-  process.env.DB_PASSWORD || '',
+  process.env.DB_NAME, // || 'condom_vending',
+  process.env.DB_USER, // || 'root',
+  process.env.DB_PASSWORD, // || '',
   {
-    host: process.env.DB_HOST || 'localhost',
+    host: process.env.DB_HOST , // || 'localhost'
+    port: process.env.DB_PORT, // || 3306,
+
     dialect: 'mysql',
     logging: process.env.NODE_ENV === 'development' ? console.log : false,
     pool: {
